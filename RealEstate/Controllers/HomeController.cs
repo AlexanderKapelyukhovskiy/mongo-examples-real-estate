4a936d74624bf2a39671c182ea3363dfc1da0014
Alex Kapelyukhovsky <alkapa2005@gmail.com>
2018-03-16 16:29:11 +0200

    Add build info v2 action

--- RealEstate/Controllers/HomeController.cs
+++ RealEstate/Controllers/HomeController.cs
@@ -1,18 +1,18 @@
-﻿using MongoDB.Driver;
+﻿using MongoDB.Bson;
 using RealEstate.App_Start;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace RealEstate.Controllers
 {
     public class HomeController : Controller
     {
+        private static RealEstateContextNewApi Context = new RealEstateContextNewApi();
+
         public ActionResult Index()
         {
-            return View();
+            var buildInfoCommand = new BsonDocument("buildInfo", 1);
+            var buildInfo = Context.Database.RunCommand<BsonDocument>(buildInfoCommand);
+            return Content(buildInfo.ToJson(), "application/json");
         }
 
         public ActionResult About()
