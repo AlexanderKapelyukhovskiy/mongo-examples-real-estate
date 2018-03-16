5bb8c39dd7a45036a8db65e3ccb36e60089614ba
Alex Kapelyukhovsky <alkapa2005@gmail.com>
2018-03-16 16:16:06 +0200

    Add new Context supported v2 mongo driver

--- RealEstate/App_Start/RealEstateContext.cs
+++ RealEstate/App_Start/RealEstateContext.cs
@@ -13,12 +13,19 @@ namespace RealEstate.App_Start
             Database = server.GetDatabase(Properties.Settings.Default.RealEstateDatabaseName);
         }
 
-        public MongoCollection<Rental> Rentals
+        public MongoCollection<Rental> Rentals => Database.GetCollection<Rental>("rentals");
+    }
+
+    public class RealEstateContextNewApi
+    {
+        public IMongoDatabase Database;
+        public RealEstateContextNewApi()
         {
-            get
-            {
-                return Database.GetCollection<Rental>("rentals");
-            }
+            var client = new MongoClient(Properties.Settings.Default.RealEstateConnectionString);
+
+            Database = client.GetDatabase(Properties.Settings.Default.RealEstateDatabaseName);
         }
+
+        public IMongoCollection<Rental> Rentals => Database.GetCollection<Rental>("rentals");
     }
 }
